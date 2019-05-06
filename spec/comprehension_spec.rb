53d9c7bddf42bf200f0d58ee5bbc80ec55d897af
boruch d <dubywider@gmail.com>
2019-05-06 00:14:31 +0000

    Automatically backed up by Learn

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -1,8 +1,8 @@
 describe "GIT" do
 # describe "STAGING AND COMMITTING CHANGES:" do
   it "1. What is the syntax for staging a change in index.html if it is in the root dir?" do
-    answer = "git add c.html
-git commit -m 
+    answer = "git add 
+git commit -m "
     encoded_answer = "bf338924ddc9ff4eb0c685a794960e0c4ffade2c"
     expect(encode(answer)).to eq(encoded_answer)
   end
