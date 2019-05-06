2f3cda5a80a8378981411bc0b01cd12d836e26d4
boruch d <dubywider@gmail.com>
2019-05-06 00:20:35 +0000

    Automatically backed up by Learn

--- spec/comprehension_spec.rb
+++ spec/comprehension_spec.rb
@@ -14,7 +14,7 @@ describe "GIT" do
 
   it "3. What is the syntax for committing all staged changes with the message 
       'update link styles'?" do
-    answer = "git commit -m"
+    answer = "git commit -m 'g'"
     encoded_answer = "8d06e2760cda4755f387db0db0e6c425b2ece976"
     expect(encode(answer)).to eq(encoded_answer)
   end
